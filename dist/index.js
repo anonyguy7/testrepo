3d75f6210e778857c976c2cfbfd85d3571c4abb0
anonyguy7 <kratossaidboi@gmail.com>
2025-07-22 21:49:46 +0530

    test

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("Hello contributors!!!!!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    res.send("Hello to the Contributors!!!");
+});
+app.listen(3000, () => {
+    console.log(`backend running: http://localhost:3000`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello contributors!!!!!")
+import express from 'express';
+
+const app = express();
+
+app.get("/" , (req , res) => {
+    res.send("Hello to the Contributors!!!");
+})
+
+app.listen(3000 , () => {
+    console.log(`backend running: http://localhost:3000`)
+});
